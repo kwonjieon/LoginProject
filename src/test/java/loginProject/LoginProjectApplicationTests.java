22bf57e4ff5a46d19044b4c6193d9a9c6b9bbaed
kwonjieon <holemart@naver.com>
2024-08-15 16:12:18 +0900

    의존성 추가

---
+++ src/test/java/loginProject/LoginProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package loginProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoginProjectApplicationTests {
+    @Test
+    void conTextLoad(){
+
+    }
+}
