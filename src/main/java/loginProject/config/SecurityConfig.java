b667104c269a4c22ab051398d5ce40be67760714
kwonjieon <holemart@naver.com>
2024-08-24 16:26:57 +0900

    feat: security config 설정

---
+++ src/main/java/loginProject/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package loginProject.config;
+
+import loginProject.domain.UserRole;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                //인증
+                .antMatchers("/security-login/info").authenticated()
+                //인가
+                .antMatchers("/security-login/admin/**").hasAuthority(UserRole.ADMIN.name())
+                .anyRequest().permitAll()
+                .and()
+                //form login방식 적용
+                .formLogin()
+                //로그인 할때 사용할 파라미터들
+                .usernameParameter("loginId")
+                .passwordParameter("password")
+                .loginPage("/security-login/login")     //로그인페이지
+                .defaultSuccessUrl("/security-login")   //성공시
+                .failureUrl("/security-login/login")
+                .and()
+                .logout()
+                .logoutUrl("/security-login/logout")
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID");
+    }
+}
